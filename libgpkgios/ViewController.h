6fa8c7c2156f56590db442271711324b93dab29b
Wes Richardet <wes@tetriscodes.com>
2016-03-21 10:41:24 -0500

    Initial Commit

---
+++ libgpkgios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GPKG
+//
+//  Created by Wes Richardet on 2/11/16.
+//  Copyright © 2016 Boundless Spatial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ libgpkgios/libgpkgios.h
@@ -0,0 +1,19 @@
+//
+//  libgpkgios.h
+//  libgpkgios
+//
+//  Created by Wes Richardet on 3/16/16.
+//  Copyright © 2016 tetriscodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libgpkgios.
+FOUNDATION_EXPORT double libgpkgiosVersionNumber;
+
+//! Project version string for libgpkgios.
+FOUNDATION_EXPORT const unsigned char libgpkgiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libgpkgios/PublicHeader.h>
+
+
